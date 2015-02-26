3a57299fe3912ee7cb5cb88802451f0f43c60d5e
Chris Netz <cnetz89@gmail.com>
2015-02-26 15:42:29 -0700

    logo animation works

--- public/app/directives/myDirectives.js
+++ public/app/directives/myDirectives.js
@@ -1,17 +1,5 @@
 var app = angular.module('personalProject');
 
 app.directive('bounceDir', function () {
-	return {
-		restrict: 'AE',
-		scope: {
-            bounce: '@'
-        },
-		link: function (scope, element) {
-			element.on('mouseenter', function() {
-
-	            element.addClass(scope.bounce);
-	            console.log('in mouseenter');
-	        });
-		}
-	}
+	
 })
